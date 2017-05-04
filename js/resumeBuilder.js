f65d97baf3030a9341d55226fa1aaccbf0076434
Ambjorn <ambjorn.lundgren@outlook.com>
2017-05-04 20:39:00 +0200

    3.5 prepend and append

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,14 +6,19 @@ This is empty on purpose! Your code to build the resume will go here.
  var firstName = "james";
  var age = 32;
  console.log(firstName);*/
- var awesomeThoughts = "I am Ambjorn and I am AWESOME";
+ /*var awesomeThoughts = "I am Ambjorn and I am AWESOME";
  console.log(awesomeThoughts);
  var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
  console.log(funThoughts);
- 
+ */
  /*
  var email = "ambjorn.lundgren@iar.com";
  var newEmail = email.replace("iar", "outlook");
  console.log(email);
  console.log(newEmail);*/
- $("#main").append(funThoughts);
+ //$("#main").append(funThoughts);
+ var formattedName = HTMLheaderName.replace("%data%", "Ambj&#246;rn");
+  var formattedRole = HTMLheaderRole.replace("%data%", "Programmerare");
+ $("#header").prepend(formattedName);
+ $("#header").prepend(formattedRole);
+ 
