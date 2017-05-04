259459725e7a52f04d381b12ea4b79d606077a11
Ambjorn <ambjorn.lundgren@outlook.com>
2017-05-04 20:22:18 +0200

    Lesson 3.3, replace

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -8,3 +8,12 @@ This is empty on purpose! Your code to build the resume will go here.
  console.log(firstName);*/
  var awesomeThoughts = "I am Ambjorn and I am AWESOME";
  console.log(awesomeThoughts);
+ var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+ console.log(funThoughts);
+ 
+ /*
+ var email = "ambjorn.lundgren@iar.com";
+ var newEmail = email.replace("iar", "outlook");
+ console.log(email);
+ console.log(newEmail);*/
+ $("#main").append(funThoughts);
